25ba29550ebc3bd840f52e02276a91069e55b781
telami <907848106@qq.com>
2019-03-04 10:05:13 +0800

    [feat] drag progress bar to control music

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -30,7 +30,7 @@
           <div class="progress-wrapper">
             <span class="time time-l">{{format(currentTime)}}</span>
             <div class="progress-bar-wrapper">
-              <progress-bar :percent="percent"></progress-bar>
+              <progress-bar :percent="percent" @percentChange="onProgressBarChange"></progress-bar>
             </div>
             <span class="time time-r">{{format(currentSong.duration)}}</span>
           </div>
@@ -104,7 +104,7 @@
       disableCls() {
         return this.songReady ? '' : 'disable'
       },
-      percent(){
+      percent() {
         return this.currentTime / this.currentSong.duration
       },
       ...mapGetters([
@@ -210,6 +210,12 @@
         const minute = this._pad(interval / 60 | 0)
         const second = this._pad(interval % 60)
         return `${minute}:${second}`
+      },
+      onProgressBarChange(percent) {
+        this.$refs.audio.currentTime = this.currentSong.duration * percent
+        if (!this.playing) {
+          this.togglePlaying()
+        }
       },
       _pad(num, n = 2) {
         let len = num.toString().length
